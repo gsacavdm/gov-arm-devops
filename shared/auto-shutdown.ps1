9239fb105a65b433e666ef0d3a81faf884dbe4bf
Gerardo Saca <gsacavdm@hotmail.com>
2018-01-01 13:44:52 -0800

    First iteration on Azure Automation

--- shared/auto-shutdown.ps1
+++ shared/auto-shutdown.ps1
@@ -1,4 +1,28 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [String] $Subscription,
+
+    [Parameter(Mandatory=$true)]
+    [String] $ResourceGroup,
+
+    [Parameter(Mandatory=$true)]
+    [String] $VmName,
+
+    [Parameter(Mandatory=$false)]
+    [String] $Environment = "AzureCloud",
+)
+
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+
+Add-AzureRMAccount `
+    -ServicePrincipal `
+    -Tenant $Conn.TenantID `
+    -ApplicationId $Conn.ApplicationID `
+    -CertificateThumbprint $Conn.CertificateThumbprint `
+    -Environment $Environment `
+    -Subscription $Subscription
+
 Stop-AzureRmVM `
 		-Force `
-		-Name contoso-vm-jumpbox `
-		-ResourceGroupName contoso
+		-Name $VmName `
+		-ResourceGroupName $ResourceGroup
